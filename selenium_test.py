791cf8312b44b9c0c7c21855273bbdee54e34221
anderskate <andrey.starozhilov@mail.ru>
2020-08-01 20:00:25 +0500

    Add check for captcha occurrence in an element

--- selenium_test.py
+++ selenium_test.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+import re
 
 
 URL = 'https://cgifederal.secure.force.com'
@@ -16,17 +20,38 @@ class WebDriver():
         self.driver.quit()
 
 
+
+class wait_for_text_to_match(object):
+    def __init__(self, locator, pattern):
+        self.locator = locator
+        self.pattern = re.compile(pattern)
+
+    def __call__(self, driver):
+        try:
+            element = ec._find_element(driver, self.locator)
+            element_src = element.get_attribute('src')
+            return self.pattern.search(element_src)
+        except StaleElementReferenceException:
+            return False
+
+
 def get_captcha_base64_image(url):
     """Get captcha image in base64 format."""
     with WebDriver(webdriver.Chrome()) as driver:
 
         driver.get(url)
 
-        image_element = driver.find_element_by_id(
-            'loginPage:SiteTemplate:siteLogin:loginComponent:loginForm:theId'
-        )
+        wait = WebDriverWait(driver, 5)
 
-        _, base64_img = image_element.get_attribute('src').split(',')
+        image_element = wait.until(
+            wait_for_text_to_match(
+                (By.ID, 'loginPage:SiteTemplate:siteLogin:loginComponent:loginForm:theId'),
+                r'data:image;base64,.'
+                )
+            )
+        
+        _, base64_img = image_element.string.split(',')
+
 
     return base64_img
 
