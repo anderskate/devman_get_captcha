3806d7430517ad79bc0476859ba3a000804ded75
Andrey Starozhilov <andrey.starozhilov@mail.ru>
2020-08-03 01:12:35 +0500

    Add function to passing authorization on site

--- selenium_test.py
+++ selenium_test.py
@@ -4,26 +4,31 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
-
+from contextlib import contextmanager
 
 
 URL = 'https://cgifederal.secure.force.com'
+
 CAPTCHA_ELEMENT_ID = 'loginPage:SiteTemplate:siteLogin:loginComponent:loginForm:theId'
+EMAIL_FIELD_ELEMENT_ID = 'loginPage:SiteTemplate:siteLogin:loginComponent:loginForm:username'
+PASSWORD_FIELD_ELEMENT_ID = 'loginPage:SiteTemplate:siteLogin:loginComponent:loginForm:password'
+PRIVACY_STATEMENT_CHECKBOX_ELEMENT_NAME = 'loginPage:SiteTemplate:siteLogin:loginComponent:loginForm:j_id167'
+CAPTCHA_FIELD_ELEMENT_ID = 'loginPage:SiteTemplate:siteLogin:loginComponent:loginForm:recaptcha_response_field'
+LOGIN_BUTTON_FIELD_ID = 'loginPage:SiteTemplate:siteLogin:loginComponent:loginForm:loginButton'
+
+
 REGEX_SEARCH_CAPTCHA = r'data:image;base64,.'
 WAITING_TIME = 5
 
 
-class WebDriver():
-    """Context manager to webdriver"""
-    def __init__(self, driver):
-        self.driver = driver
-
-    def __enter__(self):
-        return self.driver
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.driver.quit()
-
+@contextmanager
+def start_driver():
+    """Run driver as context manager"""
+    driver = webdriver.Firefox()
+    try:
+        yield driver
+    finally:
+        driver.quit()
 
 
 class SearchCaptchaDataInElement():
@@ -46,9 +51,7 @@ class SearchCaptchaDataInElement():
 
 async def get_captcha_base64_image(url):
     """Get captcha image in base64 format."""
-    with WebDriver(webdriver.Firefox()) as driver:
-        await asyncio.sleep(0)
-
+    with start_driver() as driver:
         driver.get(url)
 
         wait = WebDriverWait(driver, WAITING_TIME)
@@ -59,15 +62,41 @@ async def get_captcha_base64_image(url):
                 REGEX_SEARCH_CAPTCHA
                 )
             )
-        
-        _, base64_img = image_element.string.split(',')
+
+        _ , base64_img = image_element.string.split(',')
+
+
+        await asyncio.sleep(0)
 
     return base64_img
 
 
-def pass_authorization_on_site():
-    pass
+async def pass_authorization_on_site(driver, email, password, captcha_symbols):
+    """Make authorization on the site.
+
+    Enter the necessary credentials, such as login, password 
+    and captcha for authorization on the site.
+    """
+    email_field = driver.find_element_by_id(EMAIL_FIELD_ELEMENT_ID)
+    email_field.send_keys(email)
+
+    password_field = driver.find_element_by_id(PASSWORD_FIELD_ELEMENT_ID)
+    password_field.send_keys(password)
+
+    privacy_statement_checkbox = driver.find_element_by_name(
+        PRIVACY_STATEMENT_CHECKBOX_ELEMENT_NAME
+        )
+    privacy_statement_checkbox.click()
+
+    captcha_field = driver.find_element_by_id(CAPTCHA_FIELD_ELEMENT_ID)
+    captcha_field.send_keys(captcha_symbols)
+
+    login_button = driver.find_element_by_id(LOGIN_BUTTON_FIELD_ID)
+    login_button.click()
+
+    await asyncio.sleep(0)
+    
 
 
 if __name__ == '__main__':
-    pass
+    asyncio.run(get_captcha_base64_image(URL))
